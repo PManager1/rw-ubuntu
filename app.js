d6dec224a9636a13f1e728bf5fadfadf48c6f72f
jp-ca <jp_ca@ymail.com>
2014-11-03 16:33:58 -0800

    --

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-var express = require('express');
-var sm = require('sitemap');
-
-
-var sitemap = sm.createSitemap ({
-      hostname: 'http://example.com',
-      cacheTime: 600000,        // 600 sec - cache purge period
-      urls: [
-        { url: '/page-1/',  changefreq: 'daily', priority: 0.3 },
-        { url: '/page-2/',  changefreq: 'monthly',  priority: 0.7 },
-        { url: '/page-3/' }     // changefreq: 'weekly',  priority: 0.5
-      ]
-    });
-
-
 
 
 
